4f37bf5da995e075f7e9498301e14ea88bf6049e
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-10-02 01:33:45 +0330

    chore: register new models to the admin panel

--- survey/polls/admin.py
+++ survey/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Survey, Question, Choice
+
+
+admin.site.register(Survey)
+admin.site.register(Question)
+admin.site.register(Choice)
