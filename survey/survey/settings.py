eac58bf1d8956bd1baae1ee23b56526f5219460b
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-10-04 12:56:36 +0330

    chore: use postgres database

--- survey/survey/settings.py
+++ survey/survey/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'survey.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'survey_db',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PASSWORD': '1234',
+        'PORT': '5432',
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
