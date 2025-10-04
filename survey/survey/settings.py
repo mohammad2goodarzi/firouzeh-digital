22d4386b66157a0a530bcafb287f981e7cf769d6
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-10-04 16:41:34 +0330

    chore: dockerize the project

--- survey/survey/settings.py
+++ survey/survey/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +30,7 @@ SECRET_KEY = 'django-insecure-wqsm(goxizpsde%)d%0mxsbw49i8c6_bb4eq!amoux2%0v!mm4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -80,11 +84,10 @@ WSGI_APPLICATION = 'survey.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'survey_db',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PASSWORD': '1234',
-        'PORT': '5432',
+        'NAME': env('POSTGRES_DB', default='survey_db'),
+        'USER': env('POSTGRES_USER', default='postgres'),
+        'HOST': env('POSTGRES_SERVER', default='localhost'),
+        'PASSWORD': env('POSTGRES_PASSWORD', default='1234'),
     },
 }
 
@@ -123,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
